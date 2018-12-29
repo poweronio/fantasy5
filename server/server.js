3b67906a10ea7103c480b7f091687dab5a118374
Omar Maghrabi <maghrabi.omar@gmail.com>
2018-12-29 08:41:35 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var Combinatorics = require('js-combinatorics');
+var cmb, a;
+
+var app = module.exports = loopback();
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+
+      cmb = Combinatorics.bigCombination([1,2,3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36], 5);
+while(a = cmb.next()) 
+console.log(a);
+    }
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
