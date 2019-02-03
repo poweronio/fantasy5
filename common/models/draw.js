429541d81138ed29fd7f5dc05fa4d10769c3bcaf
Omar Maghrabi <maghrabi.omar@gmail.com>
2019-02-03 14:03:32 -0500

    draws

--- common/models/draw.js
+++ common/models/draw.js
@@ -20,42 +20,43 @@ module.exports = function(Draw) {
     
     
     Draw.greet = function (cb) {
-    //     cb(null, 'Greetings... ');
+        cb(null, 'Greetings... ');
 
-    //     cmb = Combinatorics.bigCombination([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36], 5);
-    // while(a = cmb.next()) 
-    // {
-    //     drawCreate(a);
-    //     console.log(a);
-
-
-    fs.createReadStream('ff.csv')
-    .on('open', function () {
-      console.log('OPEN');
-    })
-    .pipe(csv())
-    .on('data',function(data){
-      Draw.findOne({where:{
-        n1:data.n1,
-        n2:data.n2,
-        n3:data.n3,
-        n4:data.n4,
-        n5:data.n5,
-        numbers:[data.n1,data.n2,data.n3,data.n4,data.n5],
-      }},function(err,data){
-        if(!err){
-            console.log('***');
-          console.log(data);
-        }
-      });
-    })
-    .on('end',function(results){
-        console.log(results);
-    })
-    .on('error',function(err){
-      console.log(err);
-    });
+        cmb = Combinatorics.bigCombination([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36], 5);
+    while(a = cmb.next()) 
+    {
+        drawCreate(a);
+        console.log(a);
     }
+  }
+
+    // fs.createReadStream('ff.csv')
+    // .on('open', function () {
+    //   console.log('OPEN');
+    // })
+    // .pipe(csv())
+    // .on('data',function(data){
+    //   Draw.findOne({where:{
+    //     n1:data.n1,
+    //     n2:data.n2,
+    //     n3:data.n3,
+    //     n4:data.n4,
+    //     n5:data.n5,
+    //     numbers:[data.n1,data.n2,data.n3,data.n4,data.n5],
+    //   }},function(err,data){
+    //     if(!err){
+    //         console.log('***');
+    //       console.log(data);
+    //     }
+    //   });
+    // })
+    // .on('end',function(results){
+    //     console.log(results);
+    // })
+    // .on('error',function(err){
+    //   console.log(err);
+    // });
+    // }
     
     Draw.remoteMethod('greet', {
         returns: { arg: 'greeting', type: 'string' }
--- common/models/ffdraw.js
+++ common/models/ffdraw.js
@@ -5,12 +5,16 @@ module.exports = function(Ffdraw) {
 
     function drawCreate(a,cb){
         Ffdraw.create({
-            numbers:a,
+            numbers:JSON.stringify(a),
             n1:a[0],
             n2:a[1],
             n3:a[2],
             n4:a[3],
-            n5:a[4]
+            n5:a[4],
+            d1:a[1]-a[0],
+            d2:a[2]-a[1],
+            d3:a[3]-a[2],
+            d4:a[4]-a[3]
         },cb);
     }
     
@@ -19,11 +23,10 @@ module.exports = function(Ffdraw) {
         cb(null, 'Greetings... ');
 
         cmb = Combinatorics.bigCombination([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36], 5);
-    while(a = cmb.next()) 
-    {
+    cmb.forEach(function(a){
         drawCreate(a);
         console.log(a);
-    }
+    });
     
     }
     
